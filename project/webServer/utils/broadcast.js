a89535e65f115c6d283aab32f98834cd17d90f78
yhfandmxq <yhf_94394697@qq.com>
2020-10-16 15:35:28 +0800

    数据抽象轮播图

--- project/webServer/utils/broadcast.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (eventName, data, socket, socketObj) {
-  for (let username in socketObj) {
-    if (username === socket.user.username) {
-      continue;
-    }
-    socketObj[username].emit(eventName, data);
-  }
-};
