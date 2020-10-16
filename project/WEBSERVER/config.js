1f3e0556fd83caa3c50e0ae5f703955947343189
yhfandmxq <yhf_94394697@qq.com>
2020-10-16 15:31:55 +0800

    socketio_webserver网页聊天室

---
+++ project/WEBSERVER/config.js
@@ -0,0 +1,11 @@
+const config = {
+  indexArr: ["/index.html", "/static/js/index.js", "/static/css/index.css"],
+  wwwroot: "/home/yhfandmxq/webServer/public",
+  // url: require("url"),
+  errorOuts: {
+    404: "/home/yhfandmxq/webServer/error/404.html",
+    403: "/home/yhfandmxq/webServer/error/403.html",
+  },
+};
+
+module.exports = config;
